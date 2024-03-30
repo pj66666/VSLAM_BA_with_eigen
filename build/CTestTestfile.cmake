7a57b86e0f66bbe01752c37370cd073a8a339f66
Pj <2976394767@qq.com>
2024-03-30 21:45:02 +0800

    over

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pj/pj/VSLAM_BA_with_eigen
-# Build directory: /home/pj/pj/VSLAM_BA_with_eigen/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("app")
--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pj/pj/VSLAM_BA_with_eigen/app
-# Build directory: /home/pj/pj/VSLAM_BA_with_eigen/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
